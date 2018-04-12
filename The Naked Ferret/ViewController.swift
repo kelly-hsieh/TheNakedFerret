ff0a9a7ed7d6e9b641f6c05990f9eea7ff76b5f0
Kelly Hsieh <kelly199863@gmail.com>
2018-04-12 13:45:27 -0500

    Initial Commit

---
+++ The Naked Ferret/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Naked Ferret
+//
+//  Created by Kelly Hsieh on 4/12/18.
+//  Copyright © 2018 Kelly Hsieh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
